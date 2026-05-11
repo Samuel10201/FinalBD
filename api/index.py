da1e2ba6907f8c22be14ed14c9ac050b0e85076b
Samuel10201 <sb@uninorte.edu.co>
2026-05-10 21:20:37 -0500

    Creacion de conexion con Supabase, bases html para navegador y CRUD, e index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
+from flask import Flask
+from dotenv import load_dotenv
+from routes.auth import auth_bp
 
 def create_app():
-    """Crea la app Flask, carga configuracion, registra blueprints y retorna la app."""
-    pass
+    #Crea la app Flask, carga configuracion, registra blueprints y retorna la app.
+    
+    #Carga las variables de ambiente
+    load_dotenv()
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.secret_key = os.environ['SECRET_KEY']
+    
+    app.register_blueprint(auth_bp)
+
+    return app
 
 
 app = create_app()
+
+if __name__ == '__main__':
+      app.run(debug=True)
--- models/db.py
+++ models/db.py
@@ -1,13 +1,31 @@
+"""
+Modulo que permite a Python comunicarse con el SO. Sirve para encontrar las 
+variables de entorno.
+"""
 import os
+"""
+Es un Driver. Se encarga de tomar las consultas SQL hechas desde Python y ponerlas
+en el formato que espera Postgresql. Permite la conexion al servidor con el 
+protocolo TCP.
+"""
 import psycopg2
+"""
+Es una clase tipo cursor que permite obtener los resultados de una consulta en un 
+diccionario de Python en lugar de una tupla. Mucho mejor pq en una tupla la 
+referencia son los indices, lo que es muy sensible, ya que si se cambia el orden de 
+las columnas en la tabla original o la consulta entonces habra inconsistencias.
+"""
 from psycopg2.extras import RealDictCursor
 
 
 def get_connection():
-    """Crea y retorna una conexion a PostgreSQL (Supabase). Usa DATABASE_URL del .env."""
-    pass
-
+    """
+    Crea y retorna una conexion a PostgreSQL (Supabase). Usa DATABASE_URL del .env.
+    Retorna un objeto de tipo connection, que sirve para comunicarse con la db.
+    """
+    return psycopg2.connect(os.environ['DATABASE_URL'], cursor_factory=RealDictCursor)
 
 def close_connection(conn):
     """Cierra una conexion a la base de datos de forma segura."""
-    pass
+    if conn and conn.closed == 0:
+        conn.close()
